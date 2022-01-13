8d9b57fd31eb5fd6457fa9bff102826c5e55f3ec
Irene Breider <85449717+IBreider88@users.noreply.github.com>
2022-01-13 11:40:58 +0000

    Update BurnInExoticExPVPsFirst.R

--- BurnInExoticExPVPsFirst.R
+++ BurnInExoticExPVPsFirst.R
@@ -1,4 +1,5 @@
 #Breider et al. 2022 DOI: 10.21203/rs.3.rs-1246254/v1
+#
 JOB = Sys.getenv("SGE_TASK_ID") #JOBREP 1 to 10, make sure right createdParents file is read in
 library(AlphaSimR)
 load(paste0("CreatedParentsExPVPs",JOB,".rda"))  #Read in created parents
